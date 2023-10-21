9f992bf4b1097194cc9b1d8788dfbf304ccf986d
alecondourenaxd <alecondourenaxd@gmail.com>
2023-10-21 12:57:40 -0400

    agregando placeholder y arreglando los servicios

--- react-ezrental/src/components/RentalForm/RentalForm.jsx
+++ react-ezrental/src/components/RentalForm/RentalForm.jsx
@@ -32,28 +32,28 @@ const RentalForm = () => {
     >
       <Form.Item
         label="Título de la Residencia"
-        name="title"
+        name="tituloResid"
         rules={[{ required: true, message: 'Por favor, ingresa el título de la residencia.' }]
         }
       >
-        <Input className="w-500"/>
+        <Input className="inputsize" placeholder="Introduce el título de la residencia"/>
       </Form.Item>
 
       <Form.Item
           label="Precio"
-          name="price"
+          name="precioResid"
           rules={[{ required: true, message: 'Por favor, ingresa el precio de la residencia.' },{ validator: validarinputmayor0 }]}
         >
-          <Input className="w-500" type="number" addonBefore="Bs" />
+          <Input className="inputsize" type="number" addonBefore="Bs" placeholder="Ingresa el precio de la residencia"/>
         </Form.Item>
 
       <Form.Item
         label="Tipo de Residencia"
-        name="residenceType"
+        name="tipoResid"
         rules={[{ required: true, message: 'Por favor, selecciona el tipo de residencia.' }]
         }
       >
-        <Select >
+        <Select placeholder="Selecciona el tipo de residencia">
           <Option value="casa">Casa</Option>
           <Option value="departamento">Departamento</Option>
           <Option value="habitacion">Habitacion</Option>
@@ -63,10 +63,10 @@ const RentalForm = () => {
 
       <Form.Item
           label="Tipo de Alojamiento"
-          name="accommodationType"
+          name="tipoAlojam"
           rules={[{ required: true, message: 'Por favor, selecciona el tipo de alojamiento.' }]}
         >
-          <Select>
+          <Select placeholder="Selecciona el tipo de alojamiento">
             <Option value="alojamientoCompartido">Alojamiento Compartido</Option>
             <Option value="alojamientoEntero">Alojamiento Entero</Option>
           </Select>
@@ -79,7 +79,7 @@ const RentalForm = () => {
           rules={[{ required: true, message: 'Por favor, ingresa el numero maximo de Huesped.' },{ validator: validarinputmayor0 }]
           }
         >
-          <Input className="w-500" type="number" />
+          <Input className="inputsize" type="number" placeholder="Ingresa el número máximo de huéspedes"/>
         </Form.Item>
     
       <Form.Item
@@ -88,64 +88,65 @@ const RentalForm = () => {
         rules={[{ required: true, message: 'Por favor, ingresa el país.' }]
         }
       >
-        <Input className="w-500"/>
+        <Input className="inputsize" placeholder="Ingresa el país"/>
       </Form.Item>
 
       <Form.Item
         label="Ciudad"
-        name="city"
+        name="ciudadResid"
         rules={[{ required: true, message: 'Por favor, ingresa la ciudad.' }]
         }
       >
-        <Input className="w-500"/>
+        <Input className="inputsize" placeholder="Ingresa la ciudad"/>
       </Form.Item>
 
       <Form.Item
         label="Dirección"
-        name="address"
+        name="direcResid"
         rules={[{ required: true, message: 'Por favor, ingresa la dirección.' }]
         }
       >
-        <Input className="w-500"/>
+        <Input className="inputsize" placeholder="Ingresa la dirección de la residencia"/>
       </Form.Item>
 
       <Form.Item
         label="Número de Camas"
-        name="beds"
+        name="camaResid"
         rules={[{ required: true, message: 'Por favor, ingresa el número de camas.' },{ validator: validarinputmayor0 }]
         }
       >
-        <Input className="w-500" type="number" />
+        <Input className="inputsize" type="number" placeholder="Ingresa el número de camas"/>
       </Form.Item>
 
       <Form.Item
         label="Número de Habitaciones"
-        name="rooms"
+        name="habitResid"
         rules={[{ required: true, message: 'Por favor, ingresa el número de habitaciones.' },{ validator: validarinputmayor0 }]
         }
       >
-        <Input className="w-500" type="number" />
+        <Input className="inputsize" type="number" placeholder="Ingresa el número de habitaciones"/>
       </Form.Item>
 
       <Form.Item
         label="Número de Baños"
-        name="bathrooms"
+        name="banioResid"
         rules={[{ required: true, message: 'Por favor, ingresa el número de baños.' },{ validator: validarinputmayor0 }]
         }
       >
-        <Input className="w-500" type="number" />
+        <Input className="inputsize" type="number" placeholder="Ingresa el número de baños"/>
       </Form.Item>
 
       <Form.Item
         label="Descripción del Espacio"
-        name="description"
+        name="descripResid"
         rules={[{ required: true, message: 'Por favor, ingresa una descripción del espacio.' }]
         }
       >
-        <Input.TextArea className="w-500"
+        <Input.TextArea className="inputsize"
         showCount
         maxLength={1000}
         autoSize={{ minRows: 5, maxRows: 20 }}
+        placeholder="Ingresa una descripción del espacio"
          />
       </Form.Item>
       
@@ -157,19 +158,13 @@ const RentalForm = () => {
           <div className="amenities-group">
             <div className="amenities-row">
               <Checkbox value="wifi">Wi-Fi</Checkbox>
-              <Checkbox value="laundry">Lavadora</Checkbox>
-              <Checkbox value="kitchen">Cocina</Checkbox>
-              <Checkbox value="airConditioning">Aire Acondicionado</Checkbox>
-              <Checkbox value="workspace">Zona de Trabajo</Checkbox>
-              <Checkbox value="hairDryer">Secadora de pelo</Checkbox>
+              <Checkbox value="lavadora">Lavadora</Checkbox>
+              <Checkbox value="cocina">Cocina</Checkbox>
+              <Checkbox value="aireAcond">Aire Acondicionado</Checkbox>
+              <Checkbox value="televisor">Televisor</Checkbox>
+              
             </div>
-            <div className="amenities-row">
-              <Checkbox value="kitchen">Cocina</Checkbox>
-              <Checkbox value="dryer">Secadora</Checkbox>
-              <Checkbox value="heating">Calefacción</Checkbox>
-              <Checkbox value="tv">Televisor</Checkbox>
-              <Checkbox value="iron">Plancha</Checkbox>
-            </div>
+
           </div>
           
         </Form.Item>
@@ -180,18 +175,24 @@ const RentalForm = () => {
         >
           <div className="amenities-group">
             <div className="amenities-row">
-              <Checkbox value="pool">Piscina</Checkbox>
-              <Checkbox value="freeParking">Estacionamiento gratuito</Checkbox>
-              <Checkbox value="crib">Cuna</Checkbox>
-              <Checkbox value="gym">Gimnasio</Checkbox>
-              <Checkbox value="breakfast">Desayuno</Checkbox>
-              <Checkbox value="smokingAllowed">Apto para fumadores</Checkbox>
-            </div>
-            <div className="amenities-row">
+              <Checkbox value="pscina">Piscina</Checkbox>
               <Checkbox value="jacuzzi">Jacuzzi</Checkbox>
-              <Checkbox value="electricCharger">Cargador para autos eléctricos</Checkbox>
-              <Checkbox value="grill">Parrilla</Checkbox>
-              <Checkbox value="fireplace">Chimenea interior</Checkbox>
+              <Checkbox value="estacionamiento">Estacionamiento</Checkbox>
+              <Checkbox value="gim">Gimnasio</Checkbox>
+              <Checkbox value="parrilla">Parrilla</Checkbox>
+            </div>
+          </div>
+          
+        </Form.Item>
+
+        <Form.Item
+          label="Seguridad"
+          name="features"
+        >
+          <div className="amenities-group">
+            <div className="amenities-row">
+              <Checkbox value="camaras">Cámara de seguridad</Checkbox>
+              <Checkbox value="detectorHumo">Detector de humo</Checkbox>
             </div>
           </div>
           
@@ -203,7 +204,7 @@ const RentalForm = () => {
           rules={[{ required: true, message: 'Por favor, ingresa las fechas de alquiler.' }]
         }
         >
-        <RangePicker className="w-500"
+        <RangePicker className="inputsize"
         placeholder={['Fecha Inicio', 'Fecha Fin']}
         />
         </Form.Item>
@@ -212,29 +213,31 @@ const RentalForm = () => {
         <div className="amenities-row"> 
   <Form.Item
     label="Check In"
-    name="checkIn"
+    name="checkInResid"
     rules={[{ required: true, message: 'Por favor, ingresa la hora de check-in.' }]
     }
     className="check-in-item" // Agrega una clase aquí
   >
-    <Input.TextArea className="w-500"
+    <Input.TextArea className="inputsize"
         showCount
         maxLength={1000}
         autoSize={{ minRows: 5, maxRows: 20 }}
+        placeholder="Ingresa la hora de check-in"
     />
   </Form.Item>
   </div> 
   <div className="amenities-row">       
   <Form.Item
     label="Check Out"
-    name="checkOut"
+    name="checkOutResid"
     rules={[{ required: true, message: 'Por favor, ingresa la hora de check-out.' }]
   }
   >
-    <Input.TextArea className="w-500"
+    <Input.TextArea className="inputsize"
         showCount
         maxLength={1000}
         autoSize={{ minRows: 5, maxRows: 20 }}
+        placeholder="Ingresa la hora de check-out"
     />
   </Form.Item>
 <Form.Item>
