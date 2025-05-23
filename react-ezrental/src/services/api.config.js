bb1024ad26777a722485426166f11a1757713a63
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-05-23 01:25:33 -0400

    Update baseLocalURL

--- react-ezrental/src/services/api.config.js
+++ react-ezrental/src/services/api.config.js
@@ -1,4 +1,6 @@
-export const baseLocalUrl = 'http://localhost:4000';
+// export const baseLocalUrl = 'http://localhost:4000';
+export const baseLocalUrl = 'https://ezrental.onrender.com';
+
 
 export const headers = {
     "Content-Type": "application/json",
